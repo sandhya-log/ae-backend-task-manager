399c7dec553c527670eba9c7ef86445de87a2745
sandhya-log <hellosandhya11@gmail.com>
2025-12-22 00:22:51 +0530

    Security and Hash Password

---
+++ app/dependencies/auth.py
@@ -0,0 +1,37 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from app.core.security import SECRET_KEY,ALGORITHM
+from app.database.db import get_db
+from app.database.models import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token:str = Depends(oauth2_scheme), db:Session = Depends(get_db),):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credntials",
+            )
+    
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
+    
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = "User not found",
+        )
+    
+    return user
+    
--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from app.database.db import engine
+from app.database import models
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app= FastAPI(title="Task Manager API")
 
